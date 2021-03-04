71a73243c41befbf2af73a161dffa70d80a1b61b
Oleg Peregudov <oleg.peregudov@gmail.com>
2021-03-04 19:14:11 +0300

    first commit

---
+++ resize_data_to_256_smallest.py
@@ -0,0 +1,30 @@
+import cv2
+import albumentations as A
+import os
+
+path = r'C:\Users\Oleg\Google Drive\_DS\Python\__PRACTICE\enter_ideas\data'
+read_path = os.path.join(path, 'test')
+write_path = os.path.join(path, 'test_resized')
+
+images = os.listdir(read_path)
+
+for idx, image in enumerate(images):
+
+    if images[idx][-3:] == 'jpg':
+
+        image = cv2.imread(os.path.join(read_path, image))
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+
+        transform = A.Compose([
+            A.SmallestMaxSize(256)
+        ])
+
+        transformed = transform(image=image)
+        transformed_image = transformed["image"]
+
+        cv2.imwrite(os.path.join(write_path, images[idx]), transformed_image)
+
+    if (idx+1) % 250 == 0:
+        print(f'{idx+1} images are done.')
+
+print('All done.')
